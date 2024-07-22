0ba6527b6ee3778565ab2015bdebee1cd49381a7
Benn Herrera <benn.herrera@pm.me>
2024-07-22 01:43:31 -0700

    run tests after building.

--- gen_project.sh
+++ gen_project.sh
@@ -10,8 +10,8 @@ mkdir -p build
 if ! (
   cd build && 
   cmake "${@}" ../src &&
-  cmake --build . --parallel
+  cmake --build . --parallel &&
+  cmake --build . --target run_core_suite --target run_word_db_suite
 ); then
   exit 1
 fi
-
